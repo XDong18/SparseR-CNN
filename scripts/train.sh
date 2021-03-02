c5730708ccf17570a4c356da9c239f595a860fe5
XDong18 <liuxd1215@gmail.com>
2021-03-02 15:14:25 +0800

    fix bug

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-python projects/SparseRCNN/train_net.py --num-gpus 4 \
-    --config-file projects/SparseRCNN/configs/sparsercnn.res50.100pro.3x.yaml \
-    OUTPUT_DIR ./ins_seg/
+python projects/SparseRCNN_det/train_net.py --num-gpus 4 \
+    --config-file projects/SparseRCNN_det/configs/sparsercnn.res50.100pro.3x.yaml \
+    OUTPUT_DIR ./output/det/
